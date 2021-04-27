dc7547781a59e514492ae51598d5527d25f844fd
Luis Sperotto <luis.sperotto@ambevtech.com.br>
2021-04-27 12:55:12 +0000

    Added code for stress Kubernetes HPA test

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+const sqrt = require('sqrt');
 const os = require('os');
 const http = require('http');
 
 const requestListener = function (req, res) {
+    
+    var x = 0.0001;
+
+    for (i = 0; i <= 1000000; i++){
+        x += sqrt(x);
+    }
+
     res.writeHead(200);
     res.end(' You are in container ' + os.hostname());
+
 }
 
 const server = http.createServer(requestListener);
